f1b98e8d7e9cdcd8d5fcc3694134057eaa856976
Szijártó Csaba István <csaba.szijarto03@gmail.com>
2026-03-10 15:49:10 +0100

    line implementation started

--- sources/GreenTriangle/Skeleton.cpp
+++ sources/GreenTriangle/Skeleton.cpp
@@ -58,80 +58,62 @@ const char * fragSource = R"(
 )";
 
 const int winWidth = 600, winHeight = 600;
-/*
-* SKELETON to base the labwork program on. 
-class GreenTriangleApp : public glApp {
-	Geometry<vec2>* triangle = nullptr;  // geometria
-	GPUProgram* gpuProgram = nullptr;	 // csúcspont és pixel árnyalók
-public:
-	GreenTriangleApp() : glApp("Green triangle") { }
 
-	// Inicializáció, 
-	void onInitialization() {
-		triangle = new Geometry<vec2>;
-		triangle->Vtx() = { vec2(-0.8f, -0.8f), vec2(-0.6f, 1.0f), vec2(0.8f, -0.2f) };
-		triangle->updateGPU();
-		gpuProgram = new GPUProgram(vertSource, fragSource);
-	}
-
-	// Ablak újrarajzolás
-	void onDisplay() {
-		glClearColor(0, 0, 0, 0);     // háttér szín
-		glClear(GL_COLOR_BUFFER_BIT); // rasztertár törlés
-		glViewport(0, 0, winWidth, winHeight);
-		triangle->Draw(gpuProgram, GL_TRIANGLES, vec3(0.0f, 1.0f, 0.0f));
-	}
-};
-
-GreenTriangleApp app;*/
 
 class PointCollection
 {
 	Geometry<vec3> geometry;
 public:
 	PointCollection() {}
-	void addPoint(const vec3& point)		//függévny egy pont hozzáadására, ha konkrét pontot adunk meg paraméterként
+	void addPoint(const vec3& point)
 	{
 		geometry.Vtx().push_back(point);
 		geometry.updateGPU();
 	}
-	void addPoint(float x, float y)			//pont hozzáadása két koordinátával
+	void addPoint(float x, float y)			
 	{
 		geometry.Vtx().push_back(vec3(x, y, 1.0f));
 		geometry.updateGPU();
 	}
-	void draw(GPUProgram* gpuProgram, vec3 color = vec3(1,0,0))	//pontok kirajzolása a megadott színnel
+	void draw(GPUProgram* gpuProgram, vec3 color = vec3(1,0,0))	
 	{
 		geometry.Draw(gpuProgram, GL_POINTS, color);
 	}
 	//utilities
-	int size() { return (int)geometry.Vtx().size(); }				//pontok számának lekérdezése
-	vec3 getPoint(int index) { return geometry.Vtx()[index]; }	//pont lekérdezése index alapján
-	int pickPoint(const vec3& mouse, float threshold = 0.02f) 		//egér pozíció alapján pont kiválasztása egy adott távolságon belül
+	int size() { return (int)geometry.Vtx().size(); }				
+	vec3 getPoint(int index) { return geometry.Vtx()[index]; }	
+	int pickPoint(const vec3& mouse, float threshold = 0.02f) 
 	{
-		int pickedIndex = -1;					//változó a kiválasztott pont indexének tárolására, kezdetben -1 (érvénytelen index)
-		float bestDistance = threshold;			//változó a legjobb (legkisebb) távolság tárolására, kezdetben a küszöbérték
+		int pickedIndex = -1;
+		float bestDistance = threshold;
 		for (int i = 0; i<size(); i++) {
 			vec3 point = geometry.Vtx()[i];
-			float dx = point.x - mouse.x;		//távolság az egér és a pont x koordinátája között
-			float dy = point.y - mouse.y;		//távolság az egér és a pont y koordinátája között
-			float distance = sqrt(dx * dx + dy * dy);	//távolság kiszámítása	
-			if (distance < bestDistance) {			//ha a távolság kisebb, mint az eddigi legközelebbi pont távolsága, akkor a mostani index lesz a kiválasztott
+			float dx = point.x - mouse.x;
+			float dy = point.y - mouse.y;
+			float distance = sqrt(dx * dx + dy * dy);
+			if (distance < bestDistance) {
 				pickedIndex = i;
 				bestDistance = distance;			
 			}
 		}
-		return pickedIndex;								//ha nincs pont a küszöbértéken belül, akkor a pickedIndex -1 marad és érvénytelen indexet adunk vissza
+		return pickedIndex;
 	}
 
 };
 
 class Line
 {
-	vec2 p1, p2;
+	vec3 p1, p2;
 	Geometry<vec2> geometry;
 
 public:
+	Line(vec3 point1, vec3 point2) : p1(point1), p2(point2)
+	{
+		printf("Line added\n");
+		printf("Implicit");
+
+		printf("Parametric");
+	}
 	bool containsPointNear(vec3 point, float threshold) 
 	{
 
@@ -189,7 +171,7 @@ public:
 		glClearColor(0, 0, 0, 0);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glViewport(0, 0, winWidth, winHeight);
-		scene.draw();	//we draw the scene
+		scene.draw();
 	}
 };
 LineApp app;
